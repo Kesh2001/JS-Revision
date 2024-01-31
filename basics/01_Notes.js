80a17f13ddd134cc066a0a787c8d7ccc5f7267a7
Keshav Anand <keshavanand2001@gmail.com>
2024-01-31 19:58:41 +0530

    Testing basics

---
+++ basics/01_Notes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as new version
+
+let score = "33abc"
+
+// console.log(typeof score);
+
+// let VNum= Number(score)
+// console.log(typeof VNum);
+// console.log(VNum);//NaN- Not a number; does convert the abc but as NaN
+
+// "33"=> 33
+// "33abc"=>NaN
+// true => 1, false =>0
+
+let isLogged = "1"
+let booleanlogg = Boolean(isLogged)
+console.log(booleanlogg);// so 1 is actually true
